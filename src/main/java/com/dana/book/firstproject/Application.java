68d9a27a815679a04dbbbf30a028a7b380cbcfaf
dana <dada4202@gmail.com>
2021-11-26 00:45:21 +0900

    기존 테스트에 시큐리티 적용하기

--- src/main/java/com/dana/book/firstproject/Application.java
+++ src/main/java/com/dana/book/firstproject/Application.java
@@ -2,9 +2,8 @@ package com.dana.book.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing활성화
+//@EnableJpaAuditing //JPA Auditing활성화
 //스프링부트의 자동설정, 스프링 Bean읽기와 생성 모두 자동으로 설정
 //SpringBootApplication이 있는 위치부터 설정을 읽어나가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야한다.
 @SpringBootApplication
